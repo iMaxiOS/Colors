50888ed182fe3f85d34cd7785c9a0aabf1dfa526
Macostik <y.granchenko@gmail.com>
2021-01-18 16:53:57 +0200

    create App

---
+++ Colors-LBTA/Colors-LBTA/Colors_LBTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Colors_LBTAApp.swift
+//  Colors-LBTA
+//
+//  Created by Maxim Granchenko on 18.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Colors_LBTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
